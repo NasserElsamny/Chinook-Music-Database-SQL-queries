adb6bff59b51cd6d1a8e4d21541529155497c1c1
Ahmed Nasser Elsamny <98647894+NasserElsamny@users.noreply.github.com>
2022-06-13 02:22:26 +0200

    Create Q1.sql

---
+++ Q1.sql
@@ -0,0 +1,12 @@
+Query 1: Which artitsts had most rock tracks?
+
+SELECT a.Name,
+       COUNT(*)
+FROM Artist a
+JOIN Album al ON a.ArtistId = al.ArtistId
+JOIN Track t ON t.AlbumId = al.AlbumId
+JOIN Genre g ON t.GenreId = g.GenreId
+WHERE g.Name = 'Rock'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
